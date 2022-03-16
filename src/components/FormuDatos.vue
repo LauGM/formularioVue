1467ff66a8a147a20a26d7576221407c7db49b07
Lau G.M <lmartin@animac.com.ar>
2022-03-16 10:21:04 -0300

    Primer commit

---
+++ src/components/FormuDatos.vue
@@ -0,0 +1,155 @@
+<template>
+  <div>
+    <validation-observer
+        ref="observer"
+        v-slot="{ invalid }"
+    >
+        <form @submit.prevent="submit">
+        <validation-provider
+            v-slot="{ errors }"
+            name="Name"
+            :rules="{
+                required:true,
+                regex:'^[A-Za-z]+([\ A-Za-z])',
+                max:20
+            }"
+        >
+            <v-text-field
+            v-model="name"
+            :counter="20"
+            :error-messages="errors"
+            label="Nombre"
+            required
+            ></v-text-field>
+        </validation-provider>
+        <validation-provider
+            v-slot="{ errors }"
+            name="phoneNumber"
+            :rules="{
+            required: true,
+            digits: 10,
+            regex: '^(11)\\d{8}$'
+            }"
+        >
+            <v-text-field
+            v-model="phoneNumber"
+            :counter="10"
+            :error-messages="errors"
+            label="Celular de Buenos Aires"
+            required
+            ></v-text-field>
+        </validation-provider>
+        <validation-provider
+            v-slot="{ errors }"
+            name="email"
+            rules="required|email"
+        >
+            <v-text-field
+            v-model="email"
+            :error-messages="errors"
+            label="E-mail"
+            required
+            ></v-text-field>
+        </validation-provider>
+        
+        <validation-provider
+            v-slot="{ errors }"
+            name="checkbox"
+        >
+            <v-checkbox
+            v-model="checkbox"
+            :error-messages="errors"
+            value="1"
+            label="Recibir nuestro Newsletter"
+            type="checkbox"
+            ></v-checkbox>
+        </validation-provider>
+
+        <v-btn
+            @click="armarObjeto"
+            class="mr-4"
+            type="submit"
+            :disabled="invalid"
+        >
+            submit
+        </v-btn>
+        <v-btn @click="clear">
+            clear
+        </v-btn>
+        </form>
+    </validation-observer>
+  <tabla-datos :lista="listaUsu"/>
+  </div>
+</template>
+
+<script>
+  import { required, digits, email, max, regex } from 'vee-validate/dist/rules'
+  import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
+  import TablaDatos from './TablaDatos.vue';
+
+  setInteractionMode('eager')
+
+  extend('digits', {
+    ...digits,
+    message: '{_field_} needs to be {length} digits. ({_value_})',
+  })
+
+  extend('required', {
+    ...required,
+    message: '{_field_} can not be empty',
+  })
+
+  extend('max', {
+    ...max,
+    message: '{_field_} may not be greater than {length} characters',
+  })
+
+  extend('regex', {
+    ...regex,
+    message: '{_field_} {_value_} does not match {regex}',
+  })
+
+  extend('email', {
+    ...email,
+    message: 'Email must be valid',
+  })
+
+  export default {
+    components: {
+      ValidationProvider,
+      ValidationObserver,
+      TablaDatos
+    },
+    data: () => ({
+      name: '',
+      phoneNumber: '',
+      email: '',
+      checkbox: null,
+      listaUsu:JSON.parse(localStorage.getItem("usuarios"))||[]
+    }),
+
+    methods: {
+      submit () {
+        this.$refs.observer.validate()
+      },
+      clear () {
+        this.name = ''
+        this.phoneNumber = ''
+        this.email = ''
+        this.checkbox = null
+        this.$refs.observer.reset()
+      },
+      armarObjeto(){
+          const nuevoUsu={
+              nombre:this.name,
+              celular:this.phoneNumber,
+              email:this.email,
+              suscripto:this.checkbox=="1"
+          };
+        this.listaUsu.push(nuevoUsu);
+        localStorage.setItem("usuarios",JSON.stringify(this.listaUsu));
+      }
+    },
+  }
+</script>
+
---
+++ src/components/TablaDatos.vue
@@ -0,0 +1,81 @@
+<template>
+        <v-data-table
+        :headers="headers"
+        :items="lista"
+        item-key="nombre"
+        :items-per-page="5"
+        class="elevation-1"
+        :search="busqueda"
+        :custom-filter="filtro"
+        >
+        <template v-slot:top>
+        <v-text-field
+          v-model="busqueda"
+          label="Buscar"
+          class="mx-4"
+        ></v-text-field>
+      </template>
+        </v-data-table>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Nombre',
+            align: 'start',
+            sortable: true,
+            value: 'nombre',
+          },
+          { text: 'Celular', sortable: false, value: 'celular' },
+          { text: 'Email', sortable: false, value: 'email' },
+          { text: 'Suscripto', sortable:true, value: 'suscripto' }
+        ],
+        busqueda:''
+        }
+        
+    },
+    props:{
+        lista: Array
+    },
+    methods: {
+      filtro (value, busqueda) {
+        return value != null &&
+          busqueda != null &&
+          typeof value === 'string' &&
+          value.toString().indexOf(busqueda) !== -1
+      }
+      },
+      //se crea al inicializar vue
+      beforeCreate(){
+          console.log('before create');
+      },
+      //cuando los datos ya se encuentran disponibles
+      //se crean los metodos pero aun nose accede al dom
+      created(){
+          console.log("created")
+      },
+      //antes de montar el componente
+      beforeMount(){
+          console.log("Before Mount")
+      },
+      //cuando el componente ya se muestra
+      mounted(){
+          console.log("Mounted")
+      },
+      //al detectar cambios en los datos
+      beforeUpdate(){
+          console.log("Before Update")
+      },
+      //justo antes del desmontaje aunque el componente sigue siendo funcional
+      beforeDestroy(){
+          console.log("before Destroy")
+      },
+      //cuando el componente y sus hijos fueron desmontados
+      destroyed(){
+          console.log("destroyed")
+      }
+  }
+</script>
